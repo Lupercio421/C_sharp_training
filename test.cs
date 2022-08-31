2d4477ba64e9f721f4f63740ae6f2e6f44f4bcc9
Lupercio421 <daniel.llupercio@gmail.com>
2022-08-31 16:20:42 -0400

    got this file to work on terminal vscode

--- test.cs
+++ test.cs
@@ -1,1 +1,25 @@
-"""comment
+// Hello World! program
+
+// 
+namespace HelloWorld
+{
+    class Hello{
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+//Alternate Hello World! Implementation
+
+// using System;
+// namespace HelloWorld
+// {
+//     class Hello {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello World!");
+//         }
+//     }
+// }
